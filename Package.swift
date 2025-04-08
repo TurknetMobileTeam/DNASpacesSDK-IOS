82732b97c9062f34025132ac0a3ec10de90e00b5
TurknetMobileTeam <MobilAppDeveloper@turk.net>
2025-04-08 14:02:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,24 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "OpenRoaming",
+    name: "DNASpacesSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "OpenRoaming",
-            targets: ["OpenRoaming"]),
+            targets: ["OpenRoaming"]
+        )
     ],
     targets: [
-        .binaryTarget(name: "OpenRoaming", path: "OpenRoaming.xcframework")
+        .binaryTarget(
+            name: "OpenRoaming",
+            path: "./OpenRoaming.xcframework"
+        )
     ]
 )
